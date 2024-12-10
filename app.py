e5e228c3cb57f1820e9ca067b4b32e8361130681
omk181-lab <omayeka@clemson.edu>
2024-12-09 20:29:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, jsonify
 import pickle
 import os
-
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
 app = Flask(__name__)
 
 # Load the trained model
